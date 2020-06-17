d5ca877a6821dc832caa1810d5649a8efa8f186c
Yasmin L <48965772+yasmin-liban@users.noreply.github.com>
2020-06-17 15:28:40 -0300

    Corrigindo falha de segurança

--- main/pgadmin_postgres.sh
+++ main/pgadmin_postgres.sh
@@ -7,7 +7,7 @@ docker network  create --driver bridge postgres-network
 docker run \
  --name postgres \
  --network=postgres-network \
- -e 'POSTGRES_USER=min' \
+ -e 'POSTGRES_USER=user-name' \
  -e 'POSTGRES_PASSWORD=pass' \
  -p 5432:5432 \
  -d postgres
@@ -15,7 +15,7 @@ docker run \
  --nome pgadmin \
  --network=postgres-network \
  -p 15432:80 \
- -e 'PGADMIN_DEFAULT_EMAIL=loveis4wals8@gmail.com' \ 
+ -e 'PGADMIN_DEFAULT_EMAIL=user-email8@email.com' \ 
  -e 'PGADMIN_DEFAULT_PASSWORD=pass' \
  -d dpage/pgadmin4
 
